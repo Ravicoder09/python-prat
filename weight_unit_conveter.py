13c540c1a62f31b1ed39eae31e06b2256a72f377
Ravicoder09 <sravish340@gmail.com>
2025-02-20 00:56:29 +0530

    Update weight_unit_conveter.py

--- weight_unit_conveter.py
+++ weight_unit_conveter.py
@@ -8,21 +8,3 @@ elif unit == 'lbs' or unit == 'LBS' or unit == 'LBs' or unit == 'Lbs':
     print(f'{weight2} is nyour weight in kgs')
 else:
     print('your input is not corect')
-
-
-command=""
-while command != 'quit':
-    command=input(">")
-    if command == 'start':
-        print('game is start')
-    elif command == 'stop':
-        print('game is paused')
-    elif command == 'help':
-        print('''start -  to start the game
-              stop - to pause the game 
-              quit - to end the game''')
-    elif command == 'quit':
-        print('game ends here')
-        break
-    else:
-        print('your input is wrong type help to know the available inputs')              
